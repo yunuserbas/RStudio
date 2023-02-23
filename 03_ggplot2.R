128fd5a4e2202dd65863e42e61de08021d31b795
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-23 21:02:34 +0100

    Update 03_ggplot2.R

--- 03_ggplot2.R
+++ 03_ggplot2.R
@@ -38,5 +38,15 @@ ggplot(pokemon, aes(x = HP, y = Speed, color = Type.1)) + geom_point()
 ggplot(pokemon, aes(x = Speed)) + geom_histogram(binwidth = 0.1, fill = "blue", color = "white")
 
 # Example using geom_bar():
+ggplot(pokemon, aes(x = Generation, fill = Type.1)) + geom_bar(position = "dodge")
+
+# Example using geom_boxplot():
+ggplot(pokemon, aes(x = Type.1, y = Speed)) + geom_boxplot(fill = "orange", color = "black")
+
+# Example using coord_polar():
+ggplot(pokemon, aes(x = Type.1, fill = Generation)) +
+geom_bar() +
+coord_polar()
+
 
 
