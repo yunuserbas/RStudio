f0a147c13458bc3d8cf9c79a3de60f92cd59245e
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-27 21:59:41 +0100

    Update 04_LinearRegML.R

--- 04_LinearRegML.R
+++ 04_LinearRegML.R
@@ -4,11 +4,18 @@
 
 # First : Use a linear regression to predict the price according to one variable.
 
-> setwd("C:/Users/yunus/Downloads/")
-> house_data <- read.csv("kc_house_data.csv")
-> head(house_data)
+setwd("C:/Users/yunus/Downloads/")
+house_data <- read.csv("kc_house_data.csv")
+head(house_data)
+
+lm_model <- lm(price ~ bedrooms, data = house_data)
+summary(lm_model)
+
+predict(lm_model, newdata = data.frame(bedrooms = 3))
 
 
+
+# Second : Use a linear regression to predict the price according to multiple variables.
 
 
 
