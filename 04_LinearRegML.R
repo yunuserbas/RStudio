984b81636a2eb6fcee214c0b1b28d35a317eba7b
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-27 22:00:34 +0100

    Update 04_LinearRegML.R

--- 04_LinearRegML.R
+++ 04_LinearRegML.R
@@ -17,5 +17,18 @@ predict(lm_model, newdata = data.frame(bedrooms = 3))
 
 # Second : Use a linear regression to predict the price according to multiple variables.
 
+lm_model_1 <- lm(price ~ bedrooms + bathrooms + sqft_living, data = house_data)
+summary(lm_model_1)
+
+predict(lm_model_1, newdata = data.frame(bedrooms = 3, bathrooms = 2, sqft_living = 2000))
+
+
+
+
+
+
+
+
+
 
 
