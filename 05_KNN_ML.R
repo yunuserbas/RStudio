64c8f94f3015344c8cec19e03feae8564bab5a6d
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-02-28 22:46:16 +0100

    Update 05_KNN_ML.R

--- 05_KNN_ML.R
+++ 05_KNN_ML.R
@@ -3,5 +3,8 @@
 
 # First : Find the non-legendary Pokemon that most closely resemble those that our champion normally uses. Explain your choice of number of neighbours.
 
+load("C:/Users/yunus/Downloads/pokemon.csv")
 
-
+setwd("C:/Users/yunus/Downloads/")
+pokemon <- read.csv("pokemon.csv")
+head(pokemon)
