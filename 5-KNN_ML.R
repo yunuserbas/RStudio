51791d75bbbf89bf09a81ea2c66a9309a43746a6
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-03-09 21:54:57 +0100

    Update 5-KNN_ML.R

--- 5-KNN_ML.R
+++ 5-KNN_ML.R
@@ -3,6 +3,17 @@
 
 # First : Find the non-legendary Pokemon that most closely resemble those that our champion normally uses. Explain your choice of number of neighbours.
 
+
+
+
+
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(repr)
+library(caret)
+
+
 load("C:/Users/yunus/Downloads/pokemon.csv")
 
 setwd("C:/Users/yunus/Downloads/")
@@ -14,3 +25,27 @@ str(d)
 
 d2 <- select(d, HP, Attack, Defense, Sp..Atk, Sp..Def, Speed, Generation, Legendary)
 head(d2)
+
+
+set.seed(63)
+trainIndex <- createDataPartition(d2$Legendary, p = 0.50, list = FALSE)
+
+train <- d2[ trainIndex, ]
+test <- d2[ -trainIndex, ]
+
+
+
+# KNN
+
+set.seed(63)
+knnFit <- train(Legendary ~ .,
+                data = d2,
+                method = "knn",
+                preProcess = c("center", "scale"))
+knnFit
+
+
+plot(knnFit)
+
+
+
