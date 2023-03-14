ded0b23521b8c93bcbf7f456dfcaa9c6c5ad593f
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-03-14 22:22:11 +0100

    Update 1-Intro.R

--- 1-Intro.R
+++ 1-Intro.R
@@ -62,7 +62,17 @@ if (weather == 'cloudy' & high_chance_of_rain == TRUE) {
 }
 
 
-# Mission 5: Create a function that takes in 2 numbers as parameters, and indicates whether or not they are equal.
+# Mission 7: The unique() function takes a vector argument and returns a vector with only the unique elements in that vector (removing all duplicates).
+# Call this function and pass in the argument data. Save that result inside a variable named unique_vals. Print unique_vals variable.
+
+data <- c(120,22,22,31,15,120)
+
+unique_vals <- unique(data)
+
+print(unique_vals)
+
+
+# Mission 8: Create a function that takes in 2 numbers as parameters, and indicates whether or not they are equal.
 
 
 check_equal <- function(num1, num2) {
@@ -76,7 +86,7 @@ check_equal <- function(num1, num2) {
 
 check_equal(3, 3)
 
-# Mission 6: Create a function that uses a while or for loop to display the times tables of any number from 0 to 10. 
+# Mission 9: Create a function that uses a while or for loop to display the times tables of any number from 0 to 10. 
 # Then skip every other result.
 # For example if your input is 1 it will look like this: 0, 2, 4, 6, 8, 10
 
@@ -95,7 +105,7 @@ times_tables_skip <- function(num) {
 times_tables_skip(1)
 
 
-# Mission 7: Create a while loop that displays the vowels of the longest word in English one after the other (i.e. horizontally, not one above) 
+# Mission 10: Create a while loop that displays the vowels of the longest word in English one after the other (i.e. horizontally, not one above) 
 # of the other, i.e. vertically). The longest word in English is: "antidisestablishmentarianism"
 
 
@@ -112,7 +122,7 @@ while (i <= nchar(word)) {
 }
 
 
-# Mission 8: Create a function that takes in 3 numbers as parameters, and carries out two operations (+, -, *, /).
+# Mission 11: Create a function that takes in 3 numbers as parameters, and carries out two operations (+, -, *, /).
 # Then return the result of the applied operations. You will have to factor for a situation where division by zero is attempted.
 # Display a message that indicates whether the result is positive, negative or zero.
 # Example operations: 0*32+2 = 2; 21/0+3 > "Error, you're trying to divide by zero"
