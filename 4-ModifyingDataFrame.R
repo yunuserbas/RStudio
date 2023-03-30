6e5078b78319212f8846621de095f8c9963a7ddf
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-03-30 22:57:50 +0200

    Update 4-ModifyingDataFrame.R

--- 4-ModifyingDataFrame.R
+++ 4-ModifyingDataFrame.R
@@ -15,4 +15,10 @@ dogs <- dogs %>%
    mutate(avg_height = (height_low_inches + height_high_inches)/2)
 head(dogs)
 
+# add average height, average weight and rank change columns
+dogs <- dogs %>%
+  mutate(avg_height = (height_low_inches + height_high_inches)/2)
 
+dogs <- dogs %>%
+  mutate(avg_weight = (weight_low_lbs + weight_high_lbs)/2, rank_change_13_to_16 = rank_2016 - rank_2013)
+head(dogs)
