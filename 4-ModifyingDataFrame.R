08e5a6c54a48cc020c39999fc489f9bd9c4f2e37
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-03-30 22:49:10 +0200

    Update 4-ModifyingDataFrame.R

--- 4-ModifyingDataFrame.R
+++ 4-ModifyingDataFrame.R
@@ -1,3 +1,4 @@
+
 # load libraries
 library(readr)
 library(dplyr)
@@ -5,3 +6,13 @@ library(dplyr)
 
 # load data frame
 dogs <- read_csv('dogs.csv')
+
+# inspect data frame
+head(dogs)
+
+# add average height column
+dogs <- dogs %>%
+   mutate(avg_height = (height_low_inches + height_high_inches)/2)
+head(dogs)
+
+
