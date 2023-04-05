cb27ec395ba602757aa2ace04d80bfee06d6bb17
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-04-05 22:43:39 +0200

    Update 4-ModifyingDataFrame.R

--- 4-ModifyingDataFrame.R
+++ 4-ModifyingDataFrame.R
@@ -24,4 +24,12 @@ dogs <- dogs %>%
 head(dogs)
 
 
+# Update the code in the last code block to add the columns avg_height, avg_weight, and rank_change_13_to_16 to dogs while dropping all existing columns.
+dogs <- dogs %>%
+   transmute(avg_height = (height_low_inches + height_high_inches)/2, avg_weight = (weight_low_lbs + weight_high_lbs)/2,
+        rank_change_13_to_16 = rank_2016 - rank_2013)
+head(dogs)
+
+
+# 
 
