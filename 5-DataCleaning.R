88d7355a738b81977faebba958c5f3beae5acdd1
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-04-10 23:27:02 +0200

    Update 5-DataCleaning.R

--- 5-DataCleaning.R
+++ 5-DataCleaning.R
@@ -12,3 +12,13 @@ grocery_2 <- read_csv('grocery_2.csv')
 # inspect data frames
 head(grocery_1)
 head(grocery_2)
+
+
+summary(grocery_1)
+summary(grocery_2)
+print(colnames(grocery_1))
+print(colnames(grocery_2))
+
+# clean data frame
+clean_data_frame <- 2
+
