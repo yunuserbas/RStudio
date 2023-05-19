0d154a3c90a7931c6f24b6ee8416e0e999521ef6
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-05-19 22:52:02 +0200

    Update 06-VisualizationWith.R

--- 06-VisualizationWith.R
+++ 06-VisualizationWith.R
@@ -4,3 +4,8 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 movies <- read_csv("imdb.csv")
+
+
+# Create a new variable named viz and assign it the value of a new ggplot object that you create by invoking the ggplot() call and assigning it the dataframe movies as the data argument.
+# After you’ve defined viz you need to state the variable name on a new line in order to see it.
+# Click run and watch your code render an empty canvas. Even though no data is displayed, the data is bound to the viz ggplot object!
