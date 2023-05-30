38d676dc4ece5c49b9f09a271deb29cea1668b3a
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-05-30 22:55:11 +0200

    Update 06-VisualizationWith.R

--- 06-VisualizationWith.R
+++ 06-VisualizationWith.R
@@ -53,3 +53,6 @@ viz <- ggplot(data=movies, aes(x=imdbRating, y=nrOfWins)) +
        geom_point(aes(color=nrOfGenre), alpha=0.5) +
        labs(title="Movie Ratings Vs Award Wins", subtitle="From IMDB dataset", x="Movie Rating", y="Number of Award Wins", color="Number of Genre")
 
+viz
+
+
