1da3ca4dce2d64c4d33ff152b2f12277fc32bfd9
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-06-12 13:00:07 +0200

    Update 09-JoiningTables.R

--- 09-JoiningTables.R
+++ 09-JoiningTables.R
@@ -5,3 +5,8 @@ library(dplyr)
 orders <- read_csv("orders.csv")
 customers <- read_csv("customers.csv")
 products <- read_csv("products.csv")
+
+# inspect orders, customers and products here:
+head(orders)
+head(products)
+head(customers)
