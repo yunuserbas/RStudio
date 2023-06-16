b969cb4c58822de6d24929f2a150c90aa29e5c38
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-06-16 23:43:47 +0200

    Create 10-Mean.R

---
+++ 10-Mean.R
@@ -0,0 +1,7 @@
+
+# In this exercise, you will use R to find the average age of the first four authors in Le Monde’s top 100 books.
+
+# 29, 49, 42, 43
+
+# Add the values together, and set total equal to the answer. Print total.
+
