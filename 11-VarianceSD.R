0b3a806738be3e7fdb60c0a6a045063acf6cfe5a
Yunus Erbas <110696454+yunuserbas@users.noreply.github.com>
2023-06-19 10:57:55 +0200

    Create 11-VarianceSD.R

---
+++ 11-VarianceSD.R
@@ -0,0 +1,17 @@
+
+# difference = X − μ
+
+# Let’s find the difference between each of these data points and the mean. We’ve created a variable for each difference. 
+# Start with difference_one. Change the value of difference_one — it should be equal to the first value in the data set minus mean.
+
+# load data
+grades <- c(88, 82, 85, 84, 90)
+mean <- mean(grades)
+
+# Change these five variables
+difference_one <- 88 - mean
+difference_two <- 82 - mean
+difference_three <- 85 - mean
+difference_four <- 84 - mean
+difference_five <- 90 - mean
+
